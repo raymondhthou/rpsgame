0724794448e00b5038ee93b8bafd415aaaed7ca3
hour88 <hour88@Xubuntu.myguest.virtualbox.org>
2023-11-29 16:37:51 +0000

    added function to play a single round, but with 'rock' only

--- rpsScript.js
+++ rpsScript.js
@@ -9,5 +9,14 @@ function getComputerChoice() {
 // This function will play the game 'Rock, Paper, Scissors' in one round. The user (player) and the opponent (computer) will play against each other. When the game/round ends a message will declare win, lose or tie.
 
 function rpsSingleRound (playerSelection, computerSelection) {
-
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    return "You win! Rock beats Scissors";
+} else if (playerSelection == 'rock' && computerSelection == 'paper') {
+    return "You lose! Paper beats Rock";
+} else (playerSelection == 'rock' && computerSelection == 'rock')
+    return "It's a tie!";
 }
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(rpsSingleRound(playerSelection, computerSelection));
